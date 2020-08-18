e75606e6b9d30e2a40dbad3b06af561d411de58a
Koen <keetron@fastmail.com>
2020-08-18 09:27:24 +0200

    improved code to meet specs

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -78,50 +78,50 @@ class GildedRoseTest {
 
     @Test
     void sulfurasNegativeSellInDate_noDecreasedQuality() {
-        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", -5, 80)};
+        Item[] items = new Item[]{new Item("Sulfuras, the Extinguished Hand", -5, 80)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals("Sulfuras, the Extinguished Hand", app.items[0].name);
         assertEquals(-5, app.items[0].sellIn);
         assertEquals(80, app.items[0].quality);
     }
 
     @Test
     void backstage15DaysToSellInDate_qualityIncrease1() {
-        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 15, 10)};
+        Item[] items = new Item[]{new Item("Backstage passes to a Foo Fighters concert", 15, 10)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals("Backstage passes to a Foo Fighters concert", app.items[0].name);
         assertEquals(14, app.items[0].sellIn);
         assertEquals(11, app.items[0].quality);
     }
 
     @Test
     void backstage10DaysToSellInDate_qualityIncrease2() {
-        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)};
+        Item[] items = new Item[]{new Item("Backstage passes to a Green Day concert", 10, 10)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals("Backstage passes to a Green Day concert", app.items[0].name);
         assertEquals(9, app.items[0].sellIn);
         assertEquals(12, app.items[0].quality);
     }
 
     @Test
     void backstage5DaysToSellInDate_qualityIncrease3() {
-        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)};
+        Item[] items = new Item[]{new Item("Backstage passes to a System of a Down concert", 5, 10)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals("Backstage passes to a System of a Down concert", app.items[0].name);
         assertEquals(4, app.items[0].sellIn);
         assertEquals(13, app.items[0].quality);
     }
 
     @Test
     void backstagePastSellInDate_qualityTo0() {
-        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)};
+        Item[] items = new Item[]{new Item("Backstage passes to a Rammstein concert", 0, 10)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals("Backstage passes to a Rammstein concert", app.items[0].name);
         assertEquals(-1, app.items[0].sellIn);
         assertEquals(0, app.items[0].quality);
     }
